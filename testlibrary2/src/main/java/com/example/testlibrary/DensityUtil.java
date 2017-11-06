425f855503ed3155b26f5daebc05e2137bf10d48
attrackshao <shma102@users.noreply.github.com>
2017-11-06 14:25:05 +0800

    Update and rename TestUtil.java to DensityUtil.java
    获取设备信息（屏幕宽高，状态栏高度，dp-px，px-dp）

---
+++ testlibrary2/src/main/java/com/example/testlibrary/DensityUtil.java
@@ -0,0 +1,50 @@
+package com.example.testlibrary;
+
+/**
+ * Created by YSG on 2017/11/6.
+ */
+
+public class DensityUtil {
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public static int dpToPx(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int pxTodp(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    public static int getScreenWidth(Context context) {
+        if (context == null)
+            return -1;
+        return context.getResources().getDisplayMetrics().widthPixels;
+    }
+
+    public static int getScreenHeight(Context context) {
+        if (context == null)
+            return -1;
+        return context.getResources().getDisplayMetrics().heightPixels;
+    }
+
+    public static int getBarHeight(Context context) {
+        int statusBarHeight1 = -1;
+//获取status_bar_height资源的ID
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            //根据资源ID获取响应的尺寸值
+            statusBarHeight1 = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return statusBarHeight1;
+    }
